7ebc0ba98859dd606b22145b35fa7973b3c1de21
Yoan Thirion <yoan.thirion@gmail.com>
2021-12-09 12:24:13 +0100

    add command idempotence

--- src/main/scala/bank/Account.scala
+++ src/main/scala/bank/Account.scala
@@ -3,5 +3,6 @@ package bank
 final case class Account(
     balance: Double,
     isOverdraftAuthorized: Boolean,
-    maxWithdrawal: Double
+    maxWithdrawal: Double,
+    withdraws: List[Withdraw] = List.empty[Withdraw]
 )
--- src/main/scala/bank/AccountService.scala
+++ src/main/scala/bank/AccountService.scala
@@ -2,15 +2,33 @@ package bank
 
 object AccountService {
   def withdraw(account: Account, command: Withdraw): Either[String, Account] = {
+    if (hasAlreadyBeenApplied(account, command))
+      Right(account)
+    else
+      applyWithdraw(account, command)
+  }
+
+  private def applyWithdraw(
+      account: Account,
+      command: Withdraw
+  ): Either[String, Account] = {
     command.amount.value match {
       case amount if exceedMaxWithdrawal(account, amount) =>
         Left(s"Amount exceeding your limit of ${account.maxWithdrawal}")
       case amount if exceedBalance(account, amount) =>
         Left(s"Insufficient balance to withdraw : $amount")
       case _ =>
-        Right(account.copy(balance = account.balance - command.amount.value))
+        Right(
+          account.copy(
+            balance = account.balance - command.amount.value,
+            withdraws = account.withdraws :+ command
+          )
+        )
     }
   }
+
+  private def hasAlreadyBeenApplied(account: Account, command: Withdraw) =
+    account.withdraws.contains(command)
 
   private def exceedMaxWithdrawal(
       account: Account,
