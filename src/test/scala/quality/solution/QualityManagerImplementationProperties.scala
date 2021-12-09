d5b74f956cebe3b4090cfcd40dc39c2208a78861
Yoan Thirion <yoan.thirion@gmail.com>
2021-12-09 17:44:14 +0100

    extract constants

--- src/test/scala/quality/solution/QualityManagerImplementationProperties.scala
+++ src/test/scala/quality/solution/QualityManagerImplementationProperties.scala
@@ -4,6 +4,11 @@ import org.scalacheck.Prop.forAll
 import org.scalacheck.{Arbitrary, Gen}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatestplus.scalacheck.Checkers
+import quality.solution.QualityManagerImplementationProperties.{
+  agedBrie,
+  concertPass,
+  sulfuras
+}
 import quality.{Item, QualityManager}
 
 import scala.collection.mutable.ListBuffer
@@ -12,9 +17,9 @@ class QualityManagerImplementationProperties extends AnyFlatSpec with Checkers {
   implicit val itemGenerator: Arbitrary[Item] = Arbitrary {
     for {
       name <- Gen.oneOf(
-        "Aged Brie",
-        "Backstage passes to a TAFKAL80ETC concert",
-        "Sulfuras, Hand of Ragnaros",
+        agedBrie,
+        concertPass,
+        sulfuras,
         "Other item name"
       )
       sellIn  <- Arbitrary.arbitrary[Int]
@@ -36,19 +41,22 @@ class QualityManagerImplementationProperties extends AnyFlatSpec with Checkers {
 }
 
 object QualityManagerImplementationProperties {
-  def updateQuality(items: Array[Item]): Array[Item] = {
+  private val agedBrie    = "Aged Brie"
+  private val concertPass = "Backstage passes to a TAFKAL80ETC concert"
+  private val sulfuras    = "Sulfuras, Hand of Ragnaros"
 
+  def updateQuality(items: Array[Item]): Array[Item] = {
     val updatedItems = ListBuffer[Item]()
 
-    for (i <- 0 until items.length) {
+    for (i <- items.indices) {
       var updatedItem = items(i)
 
       if (
-        !items(i).name.equals("Aged Brie")
-        && !items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")
+        !items(i).name.equals(agedBrie)
+        && !items(i).name.equals(concertPass)
       ) {
         if (items(i).quality > 0) {
-          if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
+          if (!items(i).name.equals(sulfuras)) {
             updatedItem = updatedItem.copy(quality = updatedItem.quality - 1)
           }
         }
@@ -56,9 +64,7 @@ object QualityManagerImplementationProperties {
         if (items(i).quality < 50) {
           updatedItem = updatedItem.copy(quality = updatedItem.quality + 1)
 
-          if (
-            items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")
-          ) {
+          if (items(i).name.equals(concertPass)) {
             if (items(i).sellIn < 11) {
               if (items(i).quality < 50) {
                 updatedItem.copy(quality = updatedItem.quality + 1)
@@ -74,17 +80,15 @@ object QualityManagerImplementationProperties {
         }
       }
 
-      if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
+      if (!items(i).name.equals(sulfuras)) {
         updatedItem = updatedItem.copy(sellIn = updatedItem.sellIn - 1)
       }
 
       if (items(i).sellIn < 0) {
-        if (!items(i).name.equals("Aged Brie")) {
-          if (
-            !items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")
-          ) {
+        if (!items(i).name.equals(agedBrie)) {
+          if (!items(i).name.equals(concertPass)) {
             if (items(i).quality > 0) {
-              if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
+              if (!items(i).name.equals(sulfuras)) {
                 updatedItem = updatedItem.copy(sellIn = updatedItem.quality - 1)
               }
             }
