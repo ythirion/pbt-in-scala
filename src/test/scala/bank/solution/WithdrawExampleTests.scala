fd58d29073c5c9e7d22d02886300bae018314c68
Yoan Thirion <yoan.thirion@gmail.com>
2021-12-10 13:42:05 +0100

    consolidated example for withdraw

--- src/test/scala/bank/solution/WithdrawExampleTests.scala
+++ src/test/scala/bank/solution/WithdrawExampleTests.scala
@@ -17,127 +17,57 @@ class WithdrawExampleTests
     with Checkers
     with EitherValues {
 
-  "withdraw on account with enough money or overdraft authorized" should "match expected balance" in {
-    forAll(`account authorized to withdraw`) {
+  "withdraw" should "pass examples" in {
+    forAll(`withdraw examples`) {
       (
           balance,
           isOverdraftAuthorized,
           maxWithdrawal,
           withdrawAmount,
-          expectedBalance
+          expectedResult
       ) =>
-        {
-          assertDebitedAccount(
+        val command = toWithdraw(withdrawAmount)
+        val debitedAccount =
+          AccountService.withdraw(
             Account(balance, isOverdraftAuthorized, maxWithdrawal),
-            withdrawAmount,
-            (withdraw, debitedAccount) => {
-              debitedAccount.value.balance == expectedBalance &&
-                debitedAccount.value.withdraws.contains(withdraw) &&
-                debitedAccount.value.withdraws.length == 1
-            }
+            command
           )
-        }
-    }
-  }
 
-  "withdraw" should "not be authorized when insufficient balance and no overdraft" in {
-    forAll(`account with insufficient balance and no overdraft`) {
-      (
-          balance,
-          maxWithdrawal,
-          withdrawAmount
-      ) =>
-        {
-          assertDebitedAccount(
-            Account(balance, isOverdraftAuthorized = false, maxWithdrawal),
-            withdrawAmount,
-            (_, debitedAccount) => {
-              debitedAccount.left.value
-                .startsWith("Insufficient balance to withdraw")
-            }
-          )
-        }
-    }
-  }
-
-  "withdraw" should "not be authorized when withdraw amount > account maxWithdrawal" in {
-    forAll(`withdraw amount > account maxWithdrawal`) {
-      (
-          balance,
-          maxWithdrawal,
-          withdrawAmount
-      ) =>
-        {
-          assertDebitedAccount(
-            Account(balance, isOverdraftAuthorized = false, maxWithdrawal),
-            withdrawAmount,
-            (_, debitedAccount) => {
-              debitedAccount.left.value
-                .startsWith("Amount exceeding your limit of")
-            }
-          )
+        expectedResult match {
+          case Left(expectedErrorMessage) =>
+            debitedAccount.left.value.startsWith(expectedErrorMessage)
+          case Right(expectedBalance) =>
+            debitedAccount.value.balance == expectedBalance &&
+              debitedAccount.value.withdraws.contains(command) &&
+              debitedAccount.value.withdraws.length == 1
         }
     }
   }
 }
 
 object WithdrawExampleTests {
-  private val `account authorized to withdraw` =
+  private val `withdraw examples` =
     Table(
       (
         "balance",
         "isOverdraftAuthorized",
         "maxWithdrawal",
         "withdrawAmount",
-        "expectedBalance"
+        "expectedResult"
       ),
-      (10_000, false, 1200, 1199.99, 8800.01),
-      (0, true, 500, 50d, -50)
+      (10_000, false, 1200, 1199.99, Right(8800.01)),
+      (0, true, 500, 50d, Right(-50)),
+      (1, false, 1200, 1199.99, Left("Insufficient balance to withdraw")),
+      (0, false, 500, 50d, Left("Insufficient balance to withdraw")),
+      (10_000, true, 1200, 1200.0001, Left("Insufficient balance to withdraw")),
+      (0, false, 500, 500d, Left("Insufficient balance to withdraw"))
     )
 
-  private val `account with insufficient balance and no overdraft` =
-    Table(
-      (
-        "balance",
-        "maxWithdrawal",
-        "withdrawAmount"
-      ),
-      (1, 1200, 1199.99),
-      (0, 500, 50d)
-    )
-
-  private val `withdraw amount > account maxWithdrawal` =
-    Table(
-      (
-        "balance",
-        "maxWithdrawal",
-        "withdrawAmount"
-      ),
-      (10_000, 1200, 1200.0001),
-      (0, 500, 500d)
-    )
-
-  private def toWithdraw(withdrawAmount: Double) = {
+  private def toWithdraw(amount: Double): Withdraw = {
     Withdraw(
       UUID.randomUUID(),
-      Amount.from(withdrawAmount).get,
+      Amount.from(amount).get,
       LocalDate.now()
     )
-  }
-
-  private def assertDebitedAccount(
-      account: Account,
-      withdrawAmount: Double,
-      assertion: (Withdraw, Either[String, Account]) => Boolean
-  ): Unit = {
-    val withdrawCommand = toWithdraw(withdrawAmount)
-    val debitedAccount =
-      AccountService
-        .withdraw(
-          account,
-          withdrawCommand
-        )
-
-    assert(assertion(withdrawCommand, debitedAccount))
   }
 }
